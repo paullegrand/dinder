7448371ae8ebd1b600c25f390d7c319296550758
Rothanak So <rothanak.hello@gmail.com>
2020-09-25 16:28:15 -0400

    Matches screen

--- models/Session.ts
+++ models/Session.ts
@@ -4,4 +4,5 @@ export interface Session {
       [user: string]: boolean;
     };
   };
+  users: string[];
 }
--- screens/MatchesScreen.tsx
+++ screens/MatchesScreen.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Text, View } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 // @ts-ignore: No declaration for .js file
 import { selectSession } from '../ducks/session';
 // @ts-ignore: No declaration for .js file
 import { selectFoodItems } from '../ducks/food';
 import { FoodItem } from '../models/FoodItem';
 import { Session } from '../models/Session';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
 
 interface Props {
   foodItems: Array<FoodItem>;
@@ -23,7 +26,7 @@ const MatchesScreen = ({ foodItems, session }: Props) => {
           Object.keys(session.swipes[food]).every(
             value => session.swipes[food][value] === true,
           ),
-        )
+      )
       : [];
 
   const matchedFoods = matchedFoodKeys && matchedFoodKeys.length > 0
@@ -31,15 +34,98 @@ const MatchesScreen = ({ foodItems, session }: Props) => {
     : [];
 
   return (
-    <View>
-      <Text>Matches!</Text>
+    <View style={styles.container}>
+      <View style={styles.cardRoot}>
+        <View style={styles.card}>
+          <View style={styles.cardTop}>
+            <Text style={styles.header}>Matches</Text>
+            <Text style={styles.subText}>
+              {matchedFoods.length > 0
+                ? `You ${session.users.length == 2 ? 'both' : 'each'} swiped right on these foods`
+                : `No matches!`}
+            </Text>
+          </View>
+          {matchedFoods.length > 0 && (
+            <ScrollView style={styles.scrollView}>
+              {matchedFoods.map(match => (
+                <View key={match.key} style={styles.match}>
+                  <Text style={styles.matchText}>{match.name}</Text>
+                </View>
+              ))}
+              <View style={styles.spacer} />
+            </ScrollView>
+          )}
+        </View>
+      </View>
     </View>
   );
 }
 
-const mapStateToProps = (state: object) =>({
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E2E2E2',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardRoot: {
+    position: 'relative',
+  },
+  card: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.15,
+    shadowRadius: 30,
+  },
+  cardTop: {
+    paddingTop: 20,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+  },
+  header: {
+    fontFamily: 'Raleway-SemiBold',
+    fontSize: 24,
+    color: '#333',
+    textAlign: 'center',
+    lineHeight: 34,
+  },
+  subText: {
+    fontFamily: 'Raleway-LightItalic',
+    fontSize: 16,
+    color: '#737373',
+    textAlign: 'center',
+  },
+  scrollView: {
+    flexGrow: 0,
+    maxHeight: SCREEN_HEIGHT - 350,
+    paddingHorizontal: 15,
+  },
+  match: {
+    width: SCREEN_WIDTH - 100,
+    backgroundColor: '#F52789',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginTop: 7,
+    borderRadius: 5,
+  },
+  matchText: {
+    color: '#FFF',
+    fontFamily: 'Raleway-SemiBold',
+    fontSize: 15,
+  },
+  spacer: {
+    height: 15,
+  },
+})
+
+const mapStateToProps = (state: object) => ({
   session: selectSession(state),
-  foodItems: selectFoodItems(state), 
+  foodItems: selectFoodItems(state),
 })
 
 export default connect(mapStateToProps)(MatchesScreen)
